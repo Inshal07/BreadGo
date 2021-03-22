4975203277a15a7d66879dc051f27ce9744c27e7
Inshal <mdinshal16@gmail.com>
2021-03-22 19:53:10 +0530

    Component and services added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { DoCharityComponent } from './do-charity/do-charity.component';
+import { LoginComponent } from './login/login.component';
+import { NavComponent } from './nav/nav.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DoCharityComponent,
+    LoginComponent,
+    NavComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/do-charity/do-charity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-do-charity',
+  templateUrl: './do-charity.component.html',
+  styleUrls: ['./do-charity.component.css']
+})
+export class DoCharityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
